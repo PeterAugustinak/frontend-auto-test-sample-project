ed6076b4c60f71ff6baf6b475dc95b3553250570
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2019-07-05 17:07:14 +0200

    Adjustement of test-runner.bat and activate.bat
    Now virtual environment and test suite can be simply run by run of test_runner.bat

--- test_runner.bat
+++ test_runner.bat
@@ -1,4 +1,3 @@
 @echo off
 cls
-cmd /k "cd /d venv\Scripts & activate & cd /d    ..\..\src & python TestSuiteRunner.py test_suite_01_admin DEV CHROME"
-cd ..
+cmd /k "cd /d venv\Scripts & activate & cd /d    ..\..\src & python TestSuiteRunner.py test_suite_01_admin DEV FIREFOX & cd /d    ..\"
