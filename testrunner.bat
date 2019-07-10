02894a2a2d0ae8955ffff78da395804f12ce8ad4
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2019-07-10 21:02:52 +0200

    testrunner.bat path to project adjustement

--- testrunner.bat
+++ testrunner.bat
@@ -4,5 +4,5 @@ cls
 IF EXIST venv-fesp (
 START /MAX cmd /k "mode con cols=145 lines=80 && cd /d venv-fesp\Scripts & activate & cd /d    ..\..\src & python TestSuiteRunner.py test_suite_fesp_01_admin DEV CHROME & cd /d    ..\"
 ) ELSE (
-START /MAX cmd /k "mode con cols=145 lines=80 && echo **** Installing environment for the project ***** & py -m venv venv-fesp & cd /d venv-fesp\Scripts & activate & cd /d ..\..\..\frontend-auto-test-sample-project & python -m pip install --upgrade pip & pip install -r requierements.txt & cls & cd /d    src & python TestSuiteRunner.py test_suite_fesp_01_admin DEV CHROME & cd /d    ..\"
+START /MAX cmd /k "mode con cols=145 lines=80 && echo **** Installing environment for the project ***** & py -m venv venv-fesp & cd /d venv-fesp\Scripts & activate & cd /d ..\..\ & python -m pip install --upgrade pip & pip install -r requierements.txt & cls & cd /d    src & python TestSuiteRunner.py test_suite_fesp_01_admin DEV CHROME & cd /d    ..\"
 )
